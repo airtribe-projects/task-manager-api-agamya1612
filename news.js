0377135f295f77c46acad8c50c9cd6ae8c2eb065
Agamya <46354525+agamya1612@users.noreply.github.com>
2025-07-14 13:38:49 +0530

    Delete news.js

--- news.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth');
-const { fetchNews } = require('../controllers/newsController');
-
-router.get('/', auth, fetchNews);
-module.exports = router;
